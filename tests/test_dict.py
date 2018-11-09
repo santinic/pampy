cd7467875b70562c861878583e5c4aa49ef22c20
Claudio Santini <hireclaudio@gmail.com>
2018-11-09 02:38:50 +0100

    dict _ extraction test added

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -7,7 +7,10 @@ class IterableTests(unittest.TestCase):
 
     def test_match_dict(self):
         self.assertEqual(match_dict({'a': _, 'b': 2}, {'a': 1, 'b': 2}), (True, [1]))
-        self.assertEqual(match_dict({'a': _, 'b': _}, {'a': 1, 'b': 2}), (True, [1, 2]))
+
+    def test_match_dict_ordering(self):
+        for i in range(100):
+            self.assertEqual(match_dict({'a': _, 'b': _}, {'a': 1, 'b': 2}), (True, [1, 2]))
 
     def test_match_asymmetric(self):
         self.assertEqual(match_dict({'a': _, 'b': 2},           {'a': 1, 'b': 2, 'c': 3}),    (True, [1]))
@@ -16,7 +19,6 @@ class IterableTests(unittest.TestCase):
     def test_match_nested(self):
         self.assertEqual(match_dict({'a': {'b': _}, 'c': _},
                                     {'a': {'b': 1}, 'c': 2}), (True, [1, 2]))
-
 
     def test_dog(self):
         pet = {'type': 'dog', 'details': {'age': 3}}
