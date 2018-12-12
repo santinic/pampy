2af678b4bc76ee1c23c8b32b7858928c9ecba5e4
Aareon Sullivan <askully13@gmail.com>
2018-12-12 07:54:55 -0800

    Minor code clean up
    This PR will contain a few commits in order to help clean up a bit of the code.
    
    Not ready for merge.

--- pampy/pampy.py
+++ pampy/pampy.py
@@ -40,9 +40,7 @@ def match_value(pattern, value) -> Tuple[bool, List]:
             return True, [value]
         else:
             return False, []
-    elif isinstance(pattern, list):
-        return match_iterable(pattern, value)
-    elif isinstance(pattern, tuple):
+    elif isinstance(pattern, (list, tuple,)):
         return match_iterable(pattern, value)
     elif isinstance(pattern, dict):
         return match_dict(pattern, value)
